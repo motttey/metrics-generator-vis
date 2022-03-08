c8061f97bb30527df2e24349dd6729023613b14a
motttey <motitago@gmail.com>
2022-03-09 08:04:56 +0900

    introduce drag/drop for WeightForm

--- src/WeightForm.tsx
+++ src/WeightForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { TextField } from '@material-ui/core';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 function WeightForm (props: any): any {
   const [data, setData] = useState<Array<number>>([]);
@@ -11,32 +12,58 @@ function WeightForm (props: any): any {
     [ props?.data ]
   )
 
+  /*
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     data[parseInt(e.target.name)] = parseFloat(e.target.value);
     props.handleWeightChange(JSON.parse(JSON.stringify(data)));
   }
+  */
+
+  const handleDragEnd = (res: any) => {
+    const tmpData = Array.from(data);
+    const [ reorderedData ] = tmpData.splice(res.source.index, 1)
+    tmpData.splice(res.destination.index, 0, reorderedData);
+
+    setData(tmpData);
+  }
 
   return (
-    <form>
-      {
-        data.map((weight: number, index: number) => {
-          return (
-            <TextField
-              onChange={handleChange}
-              style= {{
-                width: "100px",
-                margin: "5px",
-                backgroundColor: "currentColor"
-              }}
-              key={index}
-              name={index.toString()}
-              value={weight.toFixed(4)}
-              label={props.attributeLabelNameList[index] || 'unknown'}
-            />
-          )
-        })
-      }
-    </form>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="weightForm">
+          {(provided) => (
+            <form
+              className="weightForm"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {data.map((weight: number, index: number) => {
+                return (
+                  <Draggable key={index} draggableId={index.toString()} index={index}>
+                    {(provided) => (
+                      <TextField
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        // onChange={handleChange}
+                        style= {{
+                          width: "100px",
+                          margin: "5px",
+                          backgroundColor: "currentColor"
+                        }}
+                        key={index}
+                        name={index.toString()}
+                        value={weight.toFixed(4)}
+                        label={props.attributeLabelNameList[index] || 'unknown'}
+                      />
+                    )}
+                  </Draggable>
+                )}
+              )}
+              {provided.placeholder}
+            </form>
+          )}
+        </Droppable>
+      </DragDropContext>
   );
 }
 
